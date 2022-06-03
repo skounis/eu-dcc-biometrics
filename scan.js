19d7f9677c6c0c9e29607334c2a76e24b2f010ad
Stavros Kounis <skounis@gmail.com>
2022-06-03 09:55:19 +0200

    feat(scan) Improve the scan cli. Exercise both qrcode-reader and jsqr.

--- scan.js
+++ scan.js
@@ -1,37 +1,77 @@
 // Importing Required Modules
-var Jimp = require("jimp");
-var fs = require('fs')
-var QrCode = require('qrcode-reader');
-const yargs = require("yargs");
+const fs = require('fs')
+const Jimp = require("jimp");
+const jsQR = require("jsqr");
 const path = require('path');
+const QrCode = require('qrcode-reader');
+const yargs = require("yargs");
 
 const options = yargs
- .usage("Usage: -f <file> -k <pem>")
- .option("f", { alias: "file", describe: "Relative path to the qr code image file to scan", type: "string", demandOption: true })
- .argv;
+  .usage("Usage: -f <file> -k <pem>")
+  .option("f", { alias: "file", describe: "Relative path to the qr code image file to scan", type: "string", demandOption: true })
+  .argv;
 
 const fileName = options.file;
 
-// Load Image from file.
+// Load Image from file into a Buffer.
 console.log("Working in" + __dirname);
 const imagePath = path.join(__dirname, fileName);
-var buffer = fs.readFileSync(imagePath);
+const buffer = fs.readFileSync(imagePath);
 
-Jimp.read(buffer, function(err, image) {
+// Parse the the Image Buffer with Jimp.
+Jimp.read(buffer, function (err, image) {
+  if (err) {
+    console.error(err);
+  }
+  console.log('\n\nScanning with QRCode reader...');
+  scanWithQRCodeReader(image, function (value) {    
+    const payload = decode(value);
+    console.log(`Payload ASCII: \n ${payload}\n\n`);
+  });
+
+  console.log('\n\nScanning with JSQRCode reader...')
+  scanWithJSQCodeReader(image, function (value) {  
+    const payload = decode(value);
+    console.log(`Payload ASCII: \n ${payload}\n\n`);
+  })
+});
+
+/**
+ * Scan a QR Code with `qrcode-reader` and return an image object 
+ * @param {*} image 
+ * @param {*} callback 
+ */
+const scanWithQRCodeReader = function (image, callback) {
+  let qrcode = new QrCode();
+  qrcode.callback = function (err, value) {
     if (err) {
-        console.error(err);        
+      console.error(err);
+      return;
     }
-    let qrcode = new QrCode();
-    qrcode.callback = function(err, value) {
-        if (err) {
-            console.error(err);
-        }
-        const data = value.result;
-        console.log(`Payload BASE64: \n ${data}\n`);
-        const buff =  Buffer.from(data, 'base64');
-        const text = buff.toString('ascii');
-        console.log(`Payload ASCII: \n ${text}\n\n`);
-        // console.log(text);
-    };
-    qrcode.decode(image.bitmap);
- });
+    callback(value.result);
+  };
+  qrcode.decode(image.bitmap);
+}
+
+/**
+ * Scan a QR Code with `jsqr` and return an image object
+ * @param {*} image 
+ * @param {*} callback 
+ */
+const scanWithJSQCodeReader = function (image, callback) {
+  const value = jsQR(image.bitmap.data, image.bitmap.width, image.bitmap.height);
+  callback(value.data)
+}
+
+/**
+ * Convert Base64 encoded string to ascii if needed.
+ * @param {string} value - Base64 encoded string.
+ */
+const decode = function (value) {
+  const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+  if (!base64regex.test(value)) return value;
+  console.log(`Payload BASE64: \n ${value}\n`);
+  const buff = Buffer.from(value, 'base64');
+  const ascii = buff.toString('ascii');
+  return ascii;
+}
