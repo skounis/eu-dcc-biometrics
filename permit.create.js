43bb01308a5643560f480100f23fadfe3164b485
Stavros Kounis <skounis@gmail.com>
2022-07-06 16:28:14 +0200

    feat(certificate) Set meta and cleanup.

--- permit.create.js
+++ permit.create.js
@@ -6,32 +6,14 @@ const path = require('path');
 const winston = require('winston');
 
 const eudcc = require('./app/lib/dcc');
-
-// TODO: Load from the library
-const CLAIM_ISS = 1;
-const CLAIM_IAT = 6;
-const CLAIM_EXP = 4;
-const CLAIM_DCC = -260;
+const logger = require('./app/lib/logger');
 
 const options = yargs
   .usage("Usage: -f <file> -k <pem> -p <payload>")
-  .option("f", { alias: "file", describe: "Path to the file you need to sign", type: "string", demandOption: true })
+  .option("f", { alias: "file", describe: "Path to the image you need to sign", type: "string", demandOption: true })
   .option("k", { alias: "key", describe: "Path to the private key (key.pem)", type: "string", demandOption: true })
   .option("p", { alias: "payload", describe: "Path to the payload (payload.json) to attach and sign", type: "string", demandOption: true })
   .argv;
-
-// Setup Logger
-const logConfiguration = {
-  'transports': [
-    new winston.transports.File({
-      filename: 'logs/permit.create.log'
-    })
-  ],
-  'format': winston.format.printf(({ level, message }) => {
-    return `${level}: ${message}`;
-  })
-};
-const logger = winston.createLogger(logConfiguration);
 
 const fileName = options.file;
 const privateKeyPath = options.key;
@@ -44,45 +26,37 @@ const privateKey = fs.readFileSync(privateKeyPath);
 const algorithm = "RSA-SHA256";
 
 // Converting string to buffer
-const data = fs.readFileSync(fileName);
+const image = fs.readFileSync(fileName);
 
-// Sign the data and returned signature in buffer
-const signature = crypto.sign(algorithm, data, privateKey);
+// Sign the image and returned signature in buffer
+const signature = crypto.sign(algorithm, image, privateKey);
 const hex = signature.toString('hex');
 
 // Load payload from file
-let raw = fs.readFileSync(payloadPath);
-let payload = JSON.parse(raw);
+const raw = fs.readFileSync(payloadPath);
+const payload = JSON.parse(raw);
 
 payload.image = path.basename(fileName);
 payload.imageSignature = hex;
 
-logger.info('Residence Permit Payload ===');
-
-// console.log('=== Residence Permit Payload ===');
-// console.log('Payload: ');
-// console.log(JSON.stringify(payload, null, 2));
-logger.info('=== Residence Permit Payload ===');
+logger.info('=== Residence Permit ===');
 logger.info('Payload: ');
 logger.info(JSON.stringify(payload, null, 2));
 
-
 // Create Residence Permit Certificate
-// Attach payload and sing
-const certPEM = fs.readFileSync('./trust/dcc/dsc-worker.pem');
-const pkPEM = fs.readFileSync('./trust/dcc/dsc-worker.p8');
+// Attach payload and sign
+const certPEM = fs.readFileSync('./trust/dcc/dsc-worker.pem'); // Document Signing Certificate (DSC)y
+const pkPEM = fs.readFileSync('./trust/dcc/dsc-worker.p8');    // Private key
 
-// console.log('\nVerifiable Certificate: ');
 logger.info('Verifiable Certificate: ');
-// Prepare the Payload
-const certPayloadData = new Map();
-certPayloadData.set(1, payload);
 
 const certPayload = new Map();
-certPayload.set(CLAIM_ISS, 'LU');
-certPayload.set(CLAIM_EXP, 1644210000);
-certPayload.set(CLAIM_IAT, 1643197539);
-certPayload.set(CLAIM_DCC, certPayloadData);
+var sEpochNow = Math.floor((+new Date()) / 1000);
+var sEpoch4Years = Math.floor(new Date().setFullYear(new Date().getFullYear() + 4) / 1000);
+certPayload.set(eudcc.CLAIM_ISS, 'NV');         // Neverland certificate
+certPayload.set(eudcc.CLAIM_EXP, sEpoch4Years); // Expiration date
+certPayload.set(eudcc.CLAIM_IAT, sEpochNow);    // Issuing date
+certPayload.set(eudcc.CLAIM_DCC, new Map().set(1, payload));
 
 eudcc.encode(certPayload, certPEM, pkPEM).then(function (value) {
   logger.info(value);
